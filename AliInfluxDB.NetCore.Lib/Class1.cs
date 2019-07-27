1d13cf042bfee7c2c604fe0d932dcf79166bdb8f
qdjjx <qdjjx9441@sina.com>
2019-07-27 14:21:49 +0800

    first commit

---
+++ AliInfluxDB.NetCore.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AliInfluxDB.NetCore.Lib
+{
+    public class Class1
+    {
+        public string GetHello()
+        {
+            return "hello";
+        }
+    }
+}
